f7bd29fd871ce8b7d63dd550a5e2be511923456f
chikiryava <98350681+chikiryava@users.noreply.github.com>
2023-04-26 14:14:41 +0500

    Add files via upload

---
+++ pract14_3-5/pract14_3-5/Form1.Designer.cs
@@ -0,0 +1,232 @@
+﻿
+namespace pract14_3_5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.addOrExportButton = new System.Windows.Forms.Button();
+            this.numbersListBox = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.numberNTextBox = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.showPeopleButton = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listOfPeopleListBox = new System.Windows.Forms.ListBox();
+            this.pathToFileTextBox = new System.Windows.Forms.TextBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.sortAndShowTextBox = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.sortedPeopleTextBox = new System.Windows.Forms.ListBox();
+            this.pathTextBox = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.addOrExportButton);
+            this.groupBox1.Controls.Add(this.numbersListBox);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.numberNTextBox);
+            this.groupBox1.Location = new System.Drawing.Point(45, 9);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(343, 246);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "zadanie3";
+            // 
+            // addOrExportButton
+            // 
+            this.addOrExportButton.Location = new System.Drawing.Point(21, 202);
+            this.addOrExportButton.Name = "addOrExportButton";
+            this.addOrExportButton.Size = new System.Drawing.Size(123, 23);
+            this.addOrExportButton.TabIndex = 3;
+            this.addOrExportButton.Text = "Добавить значения";
+            this.addOrExportButton.UseVisualStyleBackColor = true;
+            this.addOrExportButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // numbersListBox
+            // 
+            this.numbersListBox.FormattingEnabled = true;
+            this.numbersListBox.Location = new System.Drawing.Point(21, 76);
+            this.numbersListBox.Name = "numbersListBox";
+            this.numbersListBox.Size = new System.Drawing.Size(120, 108);
+            this.numbersListBox.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(108, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Введите число n(>1)";
+            // 
+            // numberNTextBox
+            // 
+            this.numberNTextBox.Location = new System.Drawing.Point(21, 36);
+            this.numberNTextBox.Name = "numberNTextBox";
+            this.numberNTextBox.Size = new System.Drawing.Size(100, 20);
+            this.numberNTextBox.TabIndex = 0;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.showPeopleButton);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.listOfPeopleListBox);
+            this.groupBox2.Controls.Add(this.pathToFileTextBox);
+            this.groupBox2.Location = new System.Drawing.Point(448, 9);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(279, 241);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "zadanie4";
+            // 
+            // showPeopleButton
+            // 
+            this.showPeopleButton.Location = new System.Drawing.Point(30, 196);
+            this.showPeopleButton.Name = "showPeopleButton";
+            this.showPeopleButton.Size = new System.Drawing.Size(178, 34);
+            this.showPeopleButton.TabIndex = 7;
+            this.showPeopleButton.Text = "Вывести и отсортировать людей, которым меньше 40 лет";
+            this.showPeopleButton.UseVisualStyleBackColor = true;
+            this.showPeopleButton.Click += new System.EventHandler(this.showPeopleButton_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(27, 20);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(120, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Укажите путь к файлу";
+            // 
+            // listOfPeopleListBox
+            // 
+            this.listOfPeopleListBox.FormattingEnabled = true;
+            this.listOfPeopleListBox.Location = new System.Drawing.Point(6, 82);
+            this.listOfPeopleListBox.Name = "listOfPeopleListBox";
+            this.listOfPeopleListBox.Size = new System.Drawing.Size(267, 108);
+            this.listOfPeopleListBox.TabIndex = 6;
+            // 
+            // pathToFileTextBox
+            // 
+            this.pathToFileTextBox.Location = new System.Drawing.Point(30, 36);
+            this.pathToFileTextBox.Name = "pathToFileTextBox";
+            this.pathToFileTextBox.Size = new System.Drawing.Size(100, 20);
+            this.pathToFileTextBox.TabIndex = 4;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.sortAndShowTextBox);
+            this.groupBox3.Controls.Add(this.label3);
+            this.groupBox3.Controls.Add(this.sortedPeopleTextBox);
+            this.groupBox3.Controls.Add(this.pathTextBox);
+            this.groupBox3.Location = new System.Drawing.Point(258, 261);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(279, 269);
+            this.groupBox3.TabIndex = 8;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "zadanie5";
+            // 
+            // sortAndShowTextBox
+            // 
+            this.sortAndShowTextBox.Location = new System.Drawing.Point(30, 196);
+            this.sortAndShowTextBox.Name = "sortAndShowTextBox";
+            this.sortAndShowTextBox.Size = new System.Drawing.Size(142, 34);
+            this.sortAndShowTextBox.TabIndex = 7;
+            this.sortAndShowTextBox.Text = "Вывести и отсортировать людей по возрасту";
+            this.sortAndShowTextBox.UseVisualStyleBackColor = true;
+            this.sortAndShowTextBox.Click += new System.EventHandler(this.sortAndShowTextBox_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(27, 20);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(120, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Укажите путь к файлу";
+            // 
+            // sortedPeopleTextBox
+            // 
+            this.sortedPeopleTextBox.FormattingEnabled = true;
+            this.sortedPeopleTextBox.Location = new System.Drawing.Point(6, 82);
+            this.sortedPeopleTextBox.Name = "sortedPeopleTextBox";
+            this.sortedPeopleTextBox.Size = new System.Drawing.Size(267, 108);
+            this.sortedPeopleTextBox.TabIndex = 6;
+            // 
+            // pathTextBox
+            // 
+            this.pathTextBox.Location = new System.Drawing.Point(30, 36);
+            this.pathTextBox.Name = "pathTextBox";
+            this.pathTextBox.Size = new System.Drawing.Size(100, 20);
+            this.pathTextBox.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(763, 569);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ListBox numbersListBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox numberNTextBox;
+        public System.Windows.Forms.Button addOrExportButton;
+        private System.Windows.Forms.GroupBox groupBox2;
+        public System.Windows.Forms.Button showPeopleButton;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox listOfPeopleListBox;
+        private System.Windows.Forms.TextBox pathToFileTextBox;
+        private System.Windows.Forms.GroupBox groupBox3;
+        public System.Windows.Forms.Button sortAndShowTextBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListBox sortedPeopleTextBox;
+        private System.Windows.Forms.TextBox pathTextBox;
+    }
+}
+
---
+++ pract14_3-5/pract14_3-5/Form1.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace pract14_3_5
+{
+    public partial class Form1 : Form
+    {
+        private bool CheckN(string N)
+        {
+           
+                if (!int.TryParse(N, out int result)) {
+                    MessageBox.Show("Число N не является числом");
+                    return false;
+                }
+                else
+                {
+                if (result <= 1)
+                {
+                    MessageBox.Show("Число N должно быть больше одного");
+                    return false;
+                }
+                else
+                    return true;
+                }
+
+            
+        }
+        private bool CheckFile(string path)
+        {
+            if (File.Exists(path))
+            {
+                StreamReader sr = File.OpenText(path);
+                if (sr.ReadToEnd().Length != 0)
+                {
+                    return true;
+                }
+                else
+                {
+                    MessageBox.Show("Файл пуст");
+                    return false;
+                }
+            }
+            else
+            {
+                MessageBox.Show("Такого файла не существует");
+                return false;
+            }
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Queue<int> numbers = new Queue<int>();
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(addOrExportButton.Text =="Добавить значения")
+            {
+                if (CheckN(numberNTextBox.Text) == true)
+                {
+                    int N = int.Parse(numberNTextBox.Text);
+                    for(int i = 1; i <= N; i++)
+                    {
+                        numbers.Enqueue(i);
+                        numbersListBox.Items.Add(i);
+                    }
+                    addOrExportButton.Text = "Извлечь из очереди";
+                    MessageBox.Show($"Очередь чисел от 1 до {N}");
+                }
+            }
+            else
+            {
+                while (numbersListBox.Items.Count != 0)
+                {
+                    numbers.Dequeue();
+                    numbersListBox.Items.RemoveAt(0);
+                    
+                }
+            }
+        }
+
+        private void showPeopleButton_Click(object sender, EventArgs e)
+        {
+            if (CheckFile(pathToFileTextBox.Text))
+            {
+                Queue<string> peoples = new Queue<string>();
+                StreamReader sr = File.OpenText(pathToFileTextBox.Text);
+                while (!sr.EndOfStream)
+                {
+                    string[] array = sr.ReadLine().Split(' ');
+                    if (int.Parse(array[3]) < 40)
+                        peoples.Enqueue(String.Join(" ", array));
+
+                }
+                sr = File.OpenText(pathToFileTextBox.Text);
+                while (!sr.EndOfStream)
+                {
+                    string people = sr.ReadLine();
+                    if (!peoples.Contains(people))
+                        peoples.Enqueue(people);
+                }
+                listOfPeopleListBox.Items.Clear();
+                while (peoples.Count != 0)
+                {
+                    listOfPeopleListBox.Items.Add(peoples.Dequeue());
+                }
+            }
+        }
+
+        private void sortAndShowTextBox_Click(object sender, EventArgs e)
+        {
+            if (CheckFile(pathTextBox.Text))
+            {
+                int r = 0;
+                Queue<string> people = new Queue<string>();
+                StreamReader sr = File.OpenText(pathTextBox.Text);
+                while (!sr.EndOfStream)
+                {
+                    sr.ReadLine();
+                    r++;
+                }
+                string[,] array = new string[r, 5];
+                sr = File.OpenText(pathTextBox.Text);
+                int k = 0;
+                while (!sr.EndOfStream)
+                {
+                    string[] peoples = sr.ReadLine().Split(' ');
+                    for (int j = 0; j < array.GetLength(1); j++)
+                    {
+                        array[k, j] = peoples[j];
+                    }
+                    k++;
+                }
+                int index = 0;
+                int w = 0;
+                while (w < r)
+                {
+
+                    string person = string.Empty;
+                    int age = 0;
+                    for (int i = 0; i < array.GetLength(0); i++)
+                    {
+                        
+                        if (int.Parse(array[i, 3]) > age)
+                        {
+                            age = int.Parse(array[i, 3]);
+                            index = i;
+                        }
+
+                    }
+                    for(int i = 0; i < 5; i++)
+                    {
+                        person = person + $" {array[index, i]}";
+                    }
+                    sortedPeopleTextBox.Items.Add(person);
+                    array[index, 3] = "-4";
+                    w++;
+                }
+            }
+        }
+    }
+}
